640f4cc64318fb6f15346cca7e643c729dde965a
santiserena <santiserena@outlook.com.ar>
2022-10-24 18:31:19 -0300

    mongo atlas db connected

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+const mongoose = require ("mongoose");
 const routes = require("./src/routes");
 const morgan = require("morgan")
 require ('dotenv').config();
@@ -10,14 +11,21 @@ const port = process.env.PORT;
 app.listen(port, () => console.log("Listening on port", port));
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+mongoose
+  .connect(process.env.MONGO_KEY)
+  .then(() => console.log("connected to db"))
+  .catch((err: Error) => console.log(err));
 
 app.use("/", routes);
+
+/* 21 min */
