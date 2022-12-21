d7f43673c1a3c22f1ab4b3e3d51fc7f95550211b
santiserena <santiserena@outlook.com.ar>
2022-12-21 20:36:40 -0300

    firebase auth done

--- src/components/Create/Create.tsx
+++ src/components/Create/Create.tsx
@@ -1,168 +1,201 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import { auth } from "../../firebase-config";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Create(): JSX.Element {
+  const [drawing, setDrawing] = useState({
+    name: "",
+    category: "",
+    description: "",
+    date: "",
+  });
+  const [categories, setCategories] = useState([]);
+  const [imageToUpload, setImageToUpload] = useState("");
+  const [pictures, setPictures] = useState([]);
+  const [confirmDelete, setConfirmDelete] = useState("");
+  const [namePictureToDelete, setNamePictureToDelete] = useState("");
 
-    const [drawing, setDrawing] = useState({
-      name: "",
-      category: "",
-      description: "",
-      date: "",
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.currentUser?.email) {
+      localStorage.setItem("us", JSON.stringify(auth.currentUser.email));
+    }
+
+    let us = JSON.parse(localStorage.getItem("us") || "");
+    if (!us) navigate("/login");
+
+    axios
+      .get("http://localhost:3000/categories")
+      .then((result) => setCategories(result.data));
+
+    axios
+      .get("http://localhost:3000/all-drawings")
+      .then((result) => setPictures(result.data));
+  }, []);
+
+  const handleOnChange = (ev: React.ChangeEvent<HTMLInputElement>): void => {
+    setDrawing({
+      ...drawing,
+      [ev.target.name]: ev.target.value,
     });
-    const [categories, setCategories] = useState ([]);
-    const [imageToUpload, setImageToUpload] = useState ('');
-    const [pictures, setPictures] = useState([]);
-    const [confirmDelete, setConfirmDelete] = useState('');
-    const [namePictureToDelete, setNamePictureToDelete] = useState('');
+  };
 
-    useEffect(() => {
-      axios
-        .get("http://localhost:3000/categories")
-        .then((result) => setCategories(result.data));
+  const preparingImage = (ev: React.ChangeEvent<HTMLInputElement>): void => {
+    let newUrl: string = ev.target.value.replace("/view?usp=share_link", "");
+    newUrl = newUrl.replace("file/d/", "uc?export=view&id=");
 
-      axios
-        .get("http://localhost:3000/all-drawings")
-        .then((result) => setPictures(result.data));
-    }, []);
-    
-    const handleOnChange = (ev: React.ChangeEvent<HTMLInputElement>): void => {
-      setDrawing({
-        ...drawing,
-        [ev.target.name]: ev.target.value,
-      });
-    };
+    setImageToUpload(newUrl);
+  };
 
-    const preparingImage = (ev: React.ChangeEvent<HTMLInputElement>): void => {
-
-      let newUrl: string = ev.target.value.replace("/view?usp=share_link", "");
-      newUrl = newUrl.replace("file/d/", "uc?export=view&id=");
-
-      setImageToUpload(newUrl);
-    };
-
-    const upload = async () => {
-      if (
-        drawing.name &&
-        imageToUpload &&
-        drawing &&
-        drawing.category &&
-        drawing.description &&
-        drawing.date
-      ) {
-        try {
-          await axios.post("http://localhost:3000/add-drawing", {
-            ...drawing,
-            image: imageToUpload,
-          });
-          alert("Picture uploaded");
-          window.location.reload();
-        } catch (error) {
-          console.log(error);
-        }
-      } else alert("Please complete all the fields");
-    };
-
-    const erase = async (ev: React.MouseEvent<HTMLElement>, id: string, name: string) => {
-      //console.log("BORRAR-> ", id,"NOMBRE-> ", name);
-      setConfirmDelete(id);
-      setNamePictureToDelete(name);
-    };
-
-    const confirmDel = async () => {
+  const upload = async () => {
+    if (
+      drawing.name &&
+      imageToUpload &&
+      drawing &&
+      drawing.category &&
+      drawing.description &&
+      drawing.date
+    ) {
       try {
-        await axios.delete(`http://localhost:3000/erase/${confirmDelete}`);
+        await axios.post("http://localhost:3000/add-drawing", {
+          ...drawing,
+          image: imageToUpload,
+        });
+        alert("Picture uploaded");
         window.location.reload();
       } catch (error) {
         console.log(error);
       }
-    };
-    
-    return (
-      <div>
-        <h1>Welcome Capuzz! Upload a new job..</h1>
+    } else alert("Please complete all the fields");
+  };
 
-        <label>
-          <input name="name" onChange={handleOnChange} />
-          Name
-        </label>
+  const erase = async (
+    ev: React.MouseEvent<HTMLElement>,
+    id: string,
+    name: string
+  ) => {
+    setConfirmDelete(id);
+    setNamePictureToDelete(name);
+  };
 
-        <br />
+  const confirmDel = async () => {
+    try {
+      await axios.delete(`http://localhost:3000/erase/${confirmDelete}`);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        <label>
-          <input name="secondaryImages" onChange={handleOnChange} />
-          Secondary images
-        </label>
+  const signO = async () => {
+    localStorage.setItem("us", JSON.stringify(""));
 
-        <br />
+    try {
+      await signOut(auth);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        <label>
-          <input
-            type="text"
-            name="category"
-            list="category"
-            onChange={handleOnChange}
-          />
-          Category
-        </label>
-        <datalist id="category">
-          {categories.map((el: string) => (
-            <option key={el} value={el}></option>
+  return (
+    <div>
+      <h1>Welcome Capuzz! Upload a new job..</h1>
+
+      <button onClick={signO}>Sign Out</button>
+
+      <br />
+      <br />
+      <br />
+      <label>
+        <input name="name" onChange={handleOnChange} />
+        Name
+      </label>
+
+      <br />
+
+      <label>
+        <input name="secondaryImages" onChange={handleOnChange} />
+        Secondary images
+      </label>
+
+      <br />
+
+      <label>
+        <input
+          type="text"
+          name="category"
+          list="category"
+          onChange={handleOnChange}
+        />
+        Category
+      </label>
+      <datalist id="category">
+        {categories.map((el: string) => (
+          <option key={el} value={el}></option>
+        ))}
+      </datalist>
+
+      <br />
+
+      <label>
+        <input name="description" onChange={handleOnChange} />
+        Description
+      </label>
+
+      <br />
+
+      <label>
+        <input type="date" name="date" onChange={handleOnChange} />
+        Date
+      </label>
+
+      <br />
+
+      <label>
+        <input name="image" onChange={preparingImage} />
+        Google Drive image URL
+      </label>
+
+      <br />
+      <p>Preview:</p>
+      {imageToUpload ? (
+        <img src={imageToUpload} alt="Not found" width="100" />
+      ) : null}
+
+      <br />
+      <button onClick={() => upload()}>Upload</button>
+
+      {/* DELETE FEATURE */}
+      <p>_______________________________________________</p>
+      {confirmDelete ? (
+        <div>
+          <h1>
+            Are you sure you want to delete the image called{" "}
+            {namePictureToDelete}?
+          </h1>
+          <button onClick={() => confirmDel()}>Yes</button>
+          <button onClick={() => setConfirmDelete("")}>No</button>
+        </div>
+      ) : null}
+
+      {pictures.length ? (
+        <div>
+          {pictures.map((e: any) => (
+            <div key={e._id}>
+              <p>Nombre: {e.name}</p>
+              <img src={e.image} width="80" alt="image not found" />
+              <button onClick={(ev) => erase(ev, e._id, e.name)}>Erase</button>
+              <p>______________</p>
+            </div>
           ))}
-        </datalist>
-
-        <br />
-
-        <label>
-          <input name="description" onChange={handleOnChange} />
-          Description
-        </label>
-
-        <br />
-
-        <label>
-          <input type="date" name="date" onChange={handleOnChange} />
-          Date
-        </label>
-
-        <br />
-
-        <label>
-          <input name="image" onChange={preparingImage} />
-          Google Drive image URL
-        </label>
-
-        <br />
-        <p>Preview:</p>
-        {imageToUpload?<img src={imageToUpload} alt="Not found" width="100" />:null}
-
-        <br />
-        <button onClick={() => upload()}>Upload</button>
-
-        {/* DELETE FEATURE */}
-        <p>_______________________________________________</p>
-        {confirmDelete ? (
-          <div>
-            <h1>Are you sure you want to delete the image called {namePictureToDelete}?</h1>
-            <button onClick={() => confirmDel()}>Yes</button>
-            <button onClick={() => setConfirmDelete("")}>No</button>
-          </div>
-        ) : null}
-
-        {pictures.length ? (
-          <div>
-            {pictures.map((e: any) => (
-              <div key={e._id}>
-                <p>Nombre: {e.name}</p>
-                <img src={e.image} width="80" alt="image not found" />
-                <button onClick={(ev) => erase(ev, e._id, e.name)}>Erase</button>
-                <p>______________</p>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>Loading..</p>
-        )}
-      </div>
-    );
+        </div>
+      ) : (
+        <p>Loading..</p>
+      )}
+    </div>
+  );
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,29 +1,45 @@
-import { useState } from "react"
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase-config";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(): JSX.Element {
-    
-    const [user , setUser] = useState ('');
-    const [password , setPassword] = useState ('');
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
-    const log_in = () =>{
-        console.log('user-> ', user);        
-        console.log('password-> ', password);
+  let navigate = useNavigate();
+
+  const log_in = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, user, password);
+      if (auth.currentUser?.email) navigate("/admin");
+    } catch (error) {
+      alert("Please check the username and password again");
     }
-  return (
+  };
 
+  return (
     <div>
       <label>email</label>
-      <input onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => setUser(ev.target.value)}/>
+      <input
+        onChange={(ev: React.ChangeEvent<HTMLInputElement>): void =>
+          setUser(ev.target.value)
+        }
+      />
 
       <br />
 
       <label>password</label>
-      <input type='password' onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => setPassword(ev.target.value)}/>
+      <input
+        type="password"
+        onChange={(ev: React.ChangeEvent<HTMLInputElement>): void =>
+          setPassword(ev.target.value)
+        }
+      />
 
       <br />
 
       <button onClick={log_in}>Log in</button>
-
     </div>
   );
 }
