8274789005047dd0f15b338443e4ad7321cc0b3b
santiserena <santiserena@outlook.com.ar>
2022-12-29 18:20:44 -0300

    carousel

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,53 @@
+import frontImage from "../../media/Cueva.jpg";
+import frontImage2 from "../../media/Mano.jpg";
+import frontImage3 from "../../media/Principito.jpg";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+function Carrusel() {
+
+  return (
+    <div className='full-container'>
+      
+      <Carousel className='carrusel-container'>
+        <Carousel.Item className='fitten'>
+          
+            <img
+              id='carrusel-image'
+              className="d-block w-100 image-carousel"
+              src={frontImage3}
+              alt="image not found"
+            />
+          
+        </Carousel.Item>
+        <Carousel.Item>
+          
+            <img
+              id='carrusel-image'
+              className="d-block w-100 image-carousel"
+              src={frontImage2}
+              alt="image not found"
+            />
+         
+        </Carousel.Item>
+        <Carousel.Item>
+         
+            <img
+              id='carrusel-image'
+              className="d-block w-100 image-carousel"
+              src={frontImage}
+              alt="image not found"
+            />
+         
+        </Carousel.Item>
+      </Carousel>
+      {/* <div className='arrow-container-mobile'>
+        <img className='arrow-mobile' src={frontImage} alt="image not found" />
+      </div> */}
+    </div>
+
+  );
+}
+
+export default Carrusel;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,26 +1,13 @@
-import frontImage from "../../media/Cueva.jpg";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Carousel from "../Carousel/Carousel";
 
 export default function Home(): JSX.Element {
-
-const [pictures, setPictures] = useState ([]);  /* ver si algo de ts */  
-useEffect( () =>{
-  axios
-  .get('http://localhost:3000/all-drawings')
-  .then(result=>setPictures(result.data))
-},[])
-
-//console.log(pictures);
 
   return (
     <div>
       <h1>Home</h1>
       
-      
-      {/* <img src="https://drive.google.com/uc?export=view&id=1H6qtc8D2zFUWgdzHoG__OhhBH_9ducI2" width="193" alt="" /> */}
-      <img src={frontImage} width="100%" alt="image not found" />
       <h2>Latest works</h2>
+      <Carousel/>
       <h2>About Emiliano and picture</h2>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia esse,
@@ -29,15 +16,6 @@ useEffect( () =>{
       </p>
       <footer>Footer</footer>
 
-
-
-  {/*     {pictures.map((e: any) => (
-        <div key={e._id}>
-          <p>{e.name}</p>
-          <p>{e._id}</p>
-          <img src={e.image} width="100" alt="image not found" />
-        </div>
-      ))} */}
     </div>
   );
 }
