11473e2f68e80ba5d5d833226b8461717d61b0de
santiserena <santiserena@outlook.com.ar>
2023-01-05 21:27:07 -0300

    home and carousel responsive styles

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,12 +8,12 @@ export default function Home(): JSX.Element {
   return (
     <main className={st.main}>
       <Navbar/>
-      {/* <h3 className={st.smallFont, st.smalltitle1}>ARTIST</h3> */}
-      <h1 className={st.header}>EMILIANO CÁCERES</h1>
-      {/* <h3 className={st.smallFont}>STILE</h3> */}
+        <h3 className={`${st.smallFont} ${st.smalltitle1}`}>ARTIST</h3>
+        <h1 className={st.header}>EMILIANO CÁCERES</h1>
+        <h3 className={`${st.smallFont} ${st.smalltitle2}`}>STILE</h3>
       <Carousel/>
       
-      <p>
+      <p className={st.text}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia esse,
         delectus ullam ducimus sequi maiores maxime dicta culpa. Sed culpa
         ipsam, vitae facere vel voluptatem quasi rem accusamus autem aliquid!
